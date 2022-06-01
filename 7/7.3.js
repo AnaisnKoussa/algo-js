d914628cdfa5619df592d1c5784365e151f42b5f
Anaïs Koussa <anaisnkoussa@gmail.com>
2022-06-01 12:56:41 +0200

    Add corrections

--- 7/7.3.js
+++ 7/7.3.js
@@ -1,5 +1,15 @@
 const readlineSync = require("readline-sync");
 function divisor() {
-    let userNumber = readlineSync.question('Give a positive integer please');
-    if (userNumber )
-}
+    let userNumber = readlineSync.question('Give a positive integer please');//6
+        for (i = 1; i < userNumber; i ++){
+            let division = userNumber/i;
+            if ((division != 1) && (division == Math.trunc(division) && (division != userNumber))) {
+                console.log(i);
+            }
+        }  
+    }
+console.log(divisor());
+
+
+// 10/2 -> 5
+// 10/5 -> 2
--- 7/7.5.js
+++ 7/7.5.js
@@ -1,7 +1,7 @@
 /*let array = [3, 10, 5, 49, 65, 1, 18, 26];
 function sort(array) {
     let arr = [];
-    while (array.length !== 0){
+    while (array.length !== 1){
         console.log("array",array);
         console.log("arr", arr);
         if (array[0] < array[1]) {
@@ -17,16 +17,18 @@ console.log(sort(array));*/
 
 
 let array = [3, 10, 5, 49, 65, 1, 18, 26];
+let array1 = [100, 45, 10000, 10, 3];
+let array2 = [10, 100, 3, 26, 45, 3];
 function sort(array) {
     let arr = [];
     while(array.length !== 0) {
-        console.log("array", array);
-        console.log("arr", arr);
         let min = Math.min(...array);
         arr.push(min);
-        delete array[min];
+        array.splice(array.indexOf(Math.min(...array)),1);
     }
     return arr;
 }
 
 console.log(sort(array));
+console.log(sort(array1));
+console.log(sort(array2));
